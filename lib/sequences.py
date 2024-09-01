5efa301ed100d70be5b509df707f9e921c559e56
Joy Chela <joychela000@gmail.com>
2024-09-01 11:37:12 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibs = [0, 1]
+        for i in range(2, length):
+            fibs.append(fibs[-1] + fibs[-2])
+        print(fibs)
